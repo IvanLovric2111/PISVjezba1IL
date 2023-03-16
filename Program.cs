821ae020b8feac1a1368cbbc3ec48aa500dded8e
IvanLovric2111 </>
2023-03-16 17:23:45 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PISVjeyba1IL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
